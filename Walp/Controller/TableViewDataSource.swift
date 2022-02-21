033ebbc03a9e14f8a1bb1ddd67c3103a06b564d9
Ali Safari <alisafari@Alis-Mac.local>
2022-02-21 14:00:38 -0800

    Added a row with collectionView to tableView for implemention of color picker.

--- Walp/Controller/TableViewDataSource.swift
+++ Walp/Controller/TableViewDataSource.swift
@@ -14,19 +14,44 @@ class TableViewDataSource: NSObject, UITableViewDataSource {
     var cats = ["Snow", "Nature", "Night Sky", "Sunflower", "Sports", "Sea", "Jungle", "Mountain", "Beach", "City", "Car"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cats.count
+        switch section {
+        case 0:
+            return cats.count
+        default:
+            return 1
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Categories"
+        default:
+            return "Colors"
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row > photos.count-1 {
             return UITableViewCell()
         } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-            cell.titleLabel.text = cats[indexPath.row]
-            cell.layer.cornerRadius = 15.0
-            cell.layer.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
-            cell.update(displaying: nil)
-            return cell
+            switch indexPath.section {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+                cell.titleLabel.text = cats[indexPath.row]
+                cell.layer.cornerRadius = 15.0
+                cell.layer.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+                cell.update(displaying: nil)
+                return cell
+            default:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "colorsCell", for: indexPath)
+                return cell
+            }
+            
         }
     }
     
---
+++ Walp/View/TableViewCellWithCollectionView.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCellWithCollectionView.swift
+//  Walp
+//
+//  Created by Ali Safari on 2/13/22.
+//  Copyright © 2022 Ali Safari. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellWithCollectionView: UITableViewCell {
+
+
+
+}
